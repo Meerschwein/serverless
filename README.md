292faec76a0550e2260440e1eb413ae84237f7a4
Merlin Volkmer <merlin.volkmer@outlook.com>
2024-12-08 20:29:03 +0100

    load test cleanup

---
+++ README.md
@@ -0,0 +1,13 @@
+# Deplyment
+
+script: ./deploy.sh 
+
+# Load test script
+
+scipt: ./load_test.sh
+(needs to be deployed first)
+  
+
+
+# Presentation
+https://docs.google.com/presentation/d/1cNitf75sppO0yID8YlrjtWJhYXWziODkK0s4aF_t9FA/edit?usp=sharing
--- load_test.sh
+++ load_test.sh
@@ -1,15 +1,5 @@
 #! /usr/bin/env bash
 
-update kubectl
-kubectl apply -f "./stateful/ftgo-db-secret.yml"kubectl apply -f "./stateful/ftgo-mysql-deployment.yml"
-kubectl apply -f "./stateful/ftgo-zookeeper-deployment.yml"
-kubectl apply -f "./stateful/ftgo-kafka-deployment.yml"
-kubectl apply -f "./stateful/ftgo-dynamodb-local.yml"
-
-PODS=("ftgo-mysql-0" "ftgo-kafka-0" "ftgo-zookeeper-0" "ftgo-dynamodb-local")
-
-bash ./ftgo-application/deployment/kubernetes/scripts/kubernetes-wait-for-ready-pods.sh $PODS
-
 kubectl apply -f "./load-test/load_test_services/ftgo-accounting-service.yml"
 kubectl apply -f "./load-test/load_test_services/ftgo-api-gateway.yml"
 kubectl apply -f "./load-test/load_test_services/ftgo-consumer-service.yml"
@@ -18,8 +8,22 @@ kubectl apply -f "./load-test/load_test_services/ftgo-order-history-service.yml"
 kubectl apply -f "./load-test/load_test_services/ftgo-order-service.yml"
 kubectl apply -f "./load-test/load_test_services/ftgo-restaurant-service.yml"
 
-kubectl get services
-kubectl get pods
+waitUntilPodRunning() {
+    POD_NAME=$1
+
+    until kubectl get pods | grep -q $POD_NAME; do
+        echo "Waiting for pod $POD_NAME to be running..."
+        sleep 3
+    done
+}
+
+waitUntilPodRunning "ftgo-accounting-service"
+waitUntilPodRunning "ftgo-api-gateway"
+waitUntilPodRunning "ftgo-consumer-service"
+waitUntilPodRunning "ftgo-kitchen-service"
+waitUntilPodRunning "ftgo-order-history-service"
+waitUntilPodRunning "ftgo-order-service"
+waitUntilPodRunning "ftgo-restaurant-service"
 
 pushd ./load-test
 
